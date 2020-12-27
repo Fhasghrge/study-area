6f7cae4fcd2a8ed05b109131752065c0bbbf59a5
Fhasghrge <1969533391@qq.com>
2020-12-27 23:57:32 +0800

    fix tody moment

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const http = require('http')
+const moment = require('moment')
 const timeLefted = require('./timeLefted').timeLefted
 const cookie = require('./selfinfos.js').cookie
 const transporter = require('./sendEmail').transporter
 
-const url = 'http://reservelib.uestc.edu.cn/ClientWeb/pro/ajax/device.aspx?classkind=1&prop=0&title=%E9%A2%84%E7%BA%A6%E7%A0%94%E4%BF%AE%E5%AE%A4&kind_id=101710042,101710044,100547050,100547052,100647898,100656264,100647900&kinds=101710042,101710044,100547050,100547052,100647898,100656264,100647900&date=2020-12-26&fr_start=&fr_end=&act=get_rsv_sta&_nocache=1608959677732'
+const today = moment().add(1, 'day').format('YYYY-MM-DD')
+const url = `http://reservelib.uestc.edu.cn/ClientWeb/pro/ajax/device.aspx?classkind=1&prop=0&title=%E9%A2%84%E7%BA%A6%E7%A0%94%E4%BF%AE%E5%AE%A4&kind_id=101710042,101710044,100547050,100547052,100647898,100656264,100647900&kinds=101710042,101710044,100547050,100547052,100647898,100656264,100647900&date=${today}&fr_start=&fr_end=&act=get_rsv_sta&_nocache=1608959677732`
+
 http.get(url, {
   headers: {
     Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
@@ -20,7 +23,7 @@ http.get(url, {
     const jsonData = JSON.parse(html);
     const {data} = jsonData;
     const filterShahe = data.filter(item => item.name.indexOf('S-') === -1)
-    console.log(filterShahe)
+    // console.log(filterShahe)
     const formatData = filterShahe.map(item => {
       const infos = timeLefted(3,{
         name: item.name,
@@ -31,7 +34,7 @@ http.get(url, {
       return infos;
     });
     const tips = formatData.filter(item => item.length > 0)
-    console.log(tips)
+    // console.log(tips)
     const bodys = tips.toString().split(',')
     let sss = ''
     for(let i = 0; i < bodys.length; i+=2) {
